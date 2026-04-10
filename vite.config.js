7c735caf6645688a7c93721244ca916cf5807337
CfrancoChavez <franco.chvez@gmail.com>
2026-04-09 23:15:16 -0300

    changes in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/cryptodash/', // Debe coincidir exactamente con el nombre de tu repo
+  base: '/cryptodash/', 
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        details: resolve(__dirname, 'details.html'), 
+      },
+    },
+  },
 });
